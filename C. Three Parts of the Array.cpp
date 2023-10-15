75de387b1698ae3a4e631758d947e1ce89f48bb0
Ankita626 <ankitapaul081@gmail.com>
2023-10-15 22:46:17 +0600

    Add files via upload

---
+++ C. Three Parts of the Array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+const int N = 1e9+7;
+int arr[N];
+int32_t main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n; cin>>n;
+    for (int i = 1; i<=n ;i++){
+        cin>>arr[i];
+    }
+    int l = 1, r = n;
+    long long s1 = 0,s2 = 0;
+    bool ok = 0;
+    while (n--){
+        s1+=arr[l];
+        s2+=arr[n];
+        if
+    }
+return 0;
+}
+
