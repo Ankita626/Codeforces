07f1d178ecb606c4c2ce491ae94b26ca01cc6c6a
Ankita626 <ankitapaul081@gmail.com>
2023-10-15 21:54:13 +0600

    Add files via upload

---
+++ aaaa.py
@@ -0,0 +1,16 @@
+# from itertools import combinations
+# tt = int(input())
+# for _ in range(tt):
+#       s = input()
+      
+#       ones = s.count('1')
+#       zeros = s.count('0')
+#       k = min(ones,zeros)
+#       if ones==zeros and len(s)>1:
+#             print(k-1)
+#       else:
+#             print(k)
+      
+s = '1101100'
+s = s[:-1]
+print(s)
